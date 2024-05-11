50730afc13ad71fbdb3bfd8dd23f2d05641758f0
Ishyrich88 <167790636+Ishyrich88@users.noreply.github.com>
2024-05-11 01:00:16 -0400

    complete version

---
+++ app.js
@@ -0,0 +1,37 @@
+const radius = 5;
+const PI = 3.1415;
+const area = PI * radius * radius;
+const spacePerPlant = 0.8; // Corrected variable name
+const initialPlant = 20;
+const spaceMaxCapacity = area / spacePerPlant;
+
+function plantControl(weeks) {
+  const plantWeeklyGrowth = initialPlant * Math.pow(2, weeks); // Corrected the assignment operator from = to *
+
+  if (plantWeeklyGrowth > spaceMaxCapacity * 0.8) {
+    console.log(`Week ${weeks}: Pruned`);
+  } else if (
+    plantWeeklyGrowth >= spaceMaxCapacity * 0.5 &&
+    plantWeeklyGrowth <= spaceMaxCapacity * 0.8
+  ) {
+    console.log(`Week ${weeks}: Monitored.`);
+  } else {
+    console.log(`Week ${weeks}: Planted`);
+  } //
+  let additionalSpace = totalPlants * spacePerPlant - area;
+  console.log(`Additional space required after ${weeks} weeks for ${initialPlant} plants: ${additionalSpace} units`);
+
+// part 2
+  let expandedArea = area + additionalSpace;
+  let expandedRadius = Math.sqrt(expandedArea / PI);
+  console.log(`Radius of the expanded garden: ${expandedRadius} units`);
+}
+
+plantControl(1);
+plantControl(2);
+plantControl(3);
+plantControl(10); // part 2
+
+
+
+
